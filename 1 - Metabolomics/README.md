6d895a27fbc68ce6ef0ba3a84129a74707bf5c80
Yuchen Sun <kevin.sun0612@hotmail.com>
2023-02-13 13:36:40 -0500

    add p-value plots and organized folder, updated readme, add knit html

---
+++ 1 - Metabolomics/README.md
@@ -0,0 +1,12 @@
+# FIHTD Homework 1 - Metabolomics
+
+### Contents
+
+- `group5_FIHTD_HW1.Rmd`: RMarkdown file with codes and answers
+- `group5_FIHTD_HW1.html`: HTML file Knit from RMarkdown
+- `script.R`: R codes only
+
+### Required Packages
+
+- [Rcpm](https://github.com/ricoderks/Rcpm)
+- [ggplot2](https://cran.r-project.org/web/packages/ggplot2/index.html)
---
+++ 1 - Metabolomics/script.R
@@ -0,0 +1,56 @@
+# change working directory
+setwd("C:/Yuchen/WCM/Courses/2023Spring/CMPB5005/homework/FIHTD/1 - Metabolomics/")
+load("Simulated_metabolomics_data.RData")
+library(Rcpm)
+library(ggplot2)
+
+#log data, transpose matrix and covert to dataframe for boxplot function
+logged_dat <-data.frame(t(log(dat)))
+#rename colnames as 1-906
+colnames(logged_dat)<-seq(1:906)
+#create boxplot, dont plot outliers
+samples_boxplot <-boxplot(logged_dat, xlab = "Sample", ylab = "log(concentration)" , outline = FALSE)
+
+
+#quotient normalization using Dieterle, F., Ross, A., Schlotterbeck, G. & Senn, H. Probabilistic Quotient Normalization as Robust Method to Account for Dilution of Complex Biological Mixtures. Application in H1 NMR Metabonomics. Anal. Chem. 78, 4281-4290 (2006).
+normalized_dat <-pqn(dat,QC=NULL)
+#log data, transpose matrix and covert to dataframe for boxplot function
+norm_logged_dat <-data.frame(t(log(normalized_dat)))
+#rename colnames as 1-906
+colnames(norm_logged_dat)<-seq(1:906)
+#create boxplot, dont plot outliers
+norm_samples_boxplot <-boxplot(norm_logged_dat, xlab = "Sample", ylab = "log(concentration)" , outline = FALSE)
+
+
+#get indices of males and females
+males <- which(gender == 1)
+females <-which(gender ==2)
+#subset normalized matrix by male and female
+male_metabolites_norm_dat <- normalized_dat[males,]
+female_metabolites_norm_dat <-normalized_dat[females,]
+#vector to hold p values for each metabolite
+pvals <- c()
+log2fold <- c()
+#for each metabolite, do t test comparing concentation between males and females
+for (i in 1:length(colnames(normalized_dat))){
+  male_sample <- male_metabolites_norm_dat[,i]
+  female_sample <- female_metabolites_norm_dat[,i]
+  temp <- t.test(male_sample,female_sample)
+  pvals[i] <- temp$p.value
+  log2fold[i] <- log2(mean(male_sample))-log2(mean(female_sample))
+}
+
+# create dataframe for volcano plot, contains log2fold change, p-value, metabolites name, and labels for differentially expressed metabolites
+de <- data.frame(log2fold,pvals,annotations$name)
+names(de) <- c('log2FoldChange','pvalue','name')
+# categorize whether metabolites are expressed differently in M or F
+de$diffexp <- 'No Diff'
+de$diffexp[de$pvalue<0.05 & de$log2FoldChange>0] <- 'High in Male'
+de$diffexp[de$pvalue<0.05 & de$log2FoldChange<0] <- 'High in Female'
+# create labels for highly differentially expressed metabolites
+de$delabel <- NA
+de$delabel[de$diffexp!='No Diff' & abs(de$log2FoldChange)>1] <- de$name[de$diffexp!='No Diff' & abs(de$log2FoldChange)>1]
+
+ggplot(de, aes(x=pvalue)) + geom_histogram(bins=50)
+
+ggplot(data=de, aes(x=log2FoldChange,y=-log10(pvalue),col=diffexp,label=delabel)) + geom_point() + theme_minimal() + geom_text(check_overlap=T,size=3,vjust=0,nudge_y=0.25)
